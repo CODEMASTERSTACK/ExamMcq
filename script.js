d2b965c24af4311604d36c02925b52d53e999f04
Krish <91791566+CODEMASTERSTACK@users.noreply.github.com>
2024-09-25 12:06:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ const firebaseConfig = {
     messagingSenderId: "235882407822",
     appId: "1:235882407822:web:a2c9b4c9c198d700c666df",
     measurementId: "G-E325LLTF0J"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 firebase.initializeApp(firebaseConfig);
 
